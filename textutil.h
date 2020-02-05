43052db6e7041d9daf07e11fd7834f5cddb80afc
kmeow <kmeow@meow.meow>
2020-02-05 13:30:06 -0600

    Correct a comment

--- textutil.h
+++ textutil.h
@@ -3,10 +3,10 @@
 #include <stdarg.h>
 #include "charvector.h"
 
-//O(n * log(n)), generally performs better than the shortest-paths algorithm - performs better on processors with larger L1 cache
+//O(n * log(n)), generally performs better than the shortest-paths algorithm
 void ReflowTextBinary(const char* input, const size_t len, cv_t* output, const int width, unsigned char num_indent_spaces);
 
-//O(width * n), can occasionally perform better than the binary-search algorithm - performs better on processors with smaller L1 cache
+//O(width * n), can sometimes perform better than the binary-search algorithm
 void ReflowText(const char* input, const size_t len, cv_t* output, const int width, unsigned char num_indent_spaces);
 
 #endif

d58938f330d69aa6870236053383f892481a84af
kmeow <kmeow@meow.meow>
2020-06-21 12:04:32 -0500

    Insertion

--- leditor.c
+++ leditor.c
@@ -6,14 +6,19 @@
 
 char* strnstr(const char*, const char*, size_t);
 
+struct TextLine
+{
+	size_t start, length;
+};
 
 struct LineEditor
 {
 	cv_t buffer;
 
-	size_t* line_indices;
-	size_t lineidx_count;
-	size_t lineidx_reserved;
+	struct TextLine* lines;
+	size_t lines_reserved;
+	size_t lines_count;
+
 /*
   Implement a simple gap buffer.
   Line endings are known after every insertion, except after a reflow.
@@ -29,79 +34,128 @@ struct LineEditor
 int LineEditor_Init(struct LineEditor* le)
 {
 	cv_init(&le->buffer, 512);
-	le->line_indices = talloc(sizeof(size_t) * 128);
-	le->lineidx_count = 0;
-	le->lineidx_reserved = 128;
+	le->lines_reserved = 128;
+	le->lines = (struct TextLine*) talloc(sizeof(struct TextLine) * le->lines_reserved);
+	memset(le->lines, 0, sizeof(struct TextLine) * le->lines_reserved);
+	le->lines_count = 0;
+
 	return 0;
 }
 
 void LineEditor_Destroy(struct LineEditor* le)
 {
+	tfree(le->lines);
 	cv_destroy(&le->buffer);
-	tfree(le->line_indices);
 }
 
-void LineEditor_RebuildLineIndices(struct LineEditor* le)
+void LineEditor_RebuildLineIndices(struct LineEditor* le, size_t from_idx);
+
+void LineEditor_Append(struct LineEditor* le, const char* data, size_t datalen)
+{
+	cv_appendstr(&le->buffer, data, datalen);
+	LineEditor_RebuildLineIndices(le, le->lines_count);
+}
+
+void LineEditor_RebuildLineIndices(struct LineEditor* le, size_t from_idx)
 {
 	const char* buf = le->buffer.data;
-	int len = le->buffer.length;
+	size_t len = le->buffer.length;
 
 	const char* p = buf;
-	int offset = 0;
-	le->lineidx_count = 0;
-	while(p && offset < len)
+
+
+	if(from_idx > le->lines_count)
+	{
+		return;
+	}
+
+	size_t offset = le->lines[from_idx].start;
+
+	le->lines_count = from_idx;
+
+	while(p)
 	{
 		p = memchr(buf + offset, '\n', len - offset);
 		if(p)
 		{
-			if(le->lineidx_count >= le->lineidx_reserved)
+			if(le->lines_count >= le->lines_reserved)
 			{
-				le->lineidx_reserved <<= 1;
-				le->line_indices = trealloc(le->line_indices, sizeof(size_t) * le->lineidx_reserved);
+				le->lines_reserved <<= 1;
+				le->lines = trealloc(le->lines, sizeof(struct TextLine) * le->lines_reserved);
 			}
 
-			le->line_indices[le->lineidx_count++] = (p - buf);
-			printf("Offset: %d\n", (p-buf));
+			le->lines[le->lines_count].start = (0 == le->lines_count) ? 0 :
+				(le->lines[le->lines_count - 1].start + le->lines[le->lines_count - 1].length + 1);
+			le->lines[le->lines_count].length = (p - buf) - offset;
+
+			++le->lines_count;
 			offset = (p - buf) + 1;
 		}
-		else
-		{
-			break;
-		}
 	}
-
-
 }
 
-void LineEditor_Insert(struct LineEditor* le, const char* data, size_t datalen)
+void LineEditor_InsertAt(struct LineEditor* le, size_t line_idx, const char* data, size_t datalen)
 {
-	cv_appendstr(&le->buffer, data, datalen);
+
+	if(line_idx > le->lines_count)
+	{
+		LineEditor_Append(le, data, datalen);
+		return;
+	}
+
+	size_t startidx = le->lines[line_idx].start;
+	el_t* copystart = &le->buffer.data[startidx];
+	size_t copylen = le->buffer.length - startidx + 1;
+	el_t* temp = talloc(copylen + 1);
+	memcpy(temp, copystart, copylen);
+	memset(copystart, 0, copylen);
+	LineEditor_Append(le, data, datalen);
+	LineEditor_Append(le, temp, copylen + 1);
+	LineEditor_RebuildLineIndices(le, line_idx);
+
+	tfree(temp);
 }
 
 struct EditorCommand
 {
 	const char* name;
-	int (*cmdfp)(struct LineEditor*, const char*);
+	int (*cmdfp)(struct LineEditor*, const char*, size_t);
 };
 
 
-int EditorCmdQuit(struct LineEditor* pLE, const char* line)
+int EditorCmdQuit(struct LineEditor* pLE, const char* line, size_t linelen)
 {
 	return -1;
 }
 
-int EditorCmdPrint(struct LineEditor* pLE, const char* line)
+int EditorCmdPrint(struct LineEditor* pLE, const char* line, size_t linelen)
 {
-	printf("%s\n", pLE->buffer.data);
+	//("%s\n", pLE->buffer.data);
+	size_t idx = 0;
+	size_t linebuf_reserved = 512;
+	char* linebuf = talloc(linebuf_reserved);
+	for(; idx < pLE->lines_count; ++idx)
+	{
+		size_t linelen = pLE->lines[idx].length;
+		if(linebuf_reserved < linelen)
+		{
+			linebuf_reserved <<= 1;
+			linebuf = trealloc(linebuf, linebuf_reserved);
+		}
+		linebuf[linelen] = 0;
+		memcpy(linebuf, &pLE->buffer.data[pLE->lines[idx].start], linelen);
+		printf("%02lu]%s\n", idx, linebuf);
+	}
+	tfree(linebuf);
 	return 0;
 }
 
-int EditorCmdRebuildLines(struct LineEditor* pLE, const char* line)
+int EditorCmdRebuildLines(struct LineEditor* pLE, const char* line, size_t linelen)
 {
-	LineEditor_RebuildLineIndices(pLE);
+	LineEditor_RebuildLineIndices(pLE, 0);
 	size_t idx = 0;
-	for(; idx < pLE->lineidx_count; ++idx)
-		printf("%lu\n", pLE->line_indices[idx]);
+	for(; idx < pLE->lines_count; ++idx)
+		printf("%lu %lu\n", pLE->lines[idx].start, pLE->lines[idx].length);
 	return 0;
 }
 
@@ -112,12 +166,26 @@ int EditorCmdCmp(const void* pA, const void* pB)
 	return strcmp((const char*) pA, pCmd->name);
 }
 
+int EditorCmdInsert(struct LineEditor* pLE, const char* line, size_t linelen)
+{
+	const char* teststr = "test of insertion\n";
+	LineEditor_InsertAt(pLE, 0, teststr, strlen(teststr));
+	return 0;
+}
+
+void Lexer_LexString(const char* str)
+{
+
+}
+
+
 int main(void)
 {
 	struct LineEditor editor;
 
 	struct EditorCommand commands[] =
 		{
+			{"i", EditorCmdInsert},
 			{"p", EditorCmdPrint},
 			{"q", EditorCmdQuit},
 			{"r", EditorCmdRebuildLines}
@@ -126,22 +194,26 @@ int main(void)
 
 	LineEditor_Init(&editor);
 
-	size_t linelimit = 256;
-	char* buf = malloc(linelimit);
-	size_t sz = 0;
+	const size_t linelimit = 256; //A hard limit we impose on the user
+	size_t linelen = 256; //A flexible limit used by getline()
+
+	char* buf = talloc(linelimit);
+	memset(buf, 0, linelimit);
 
 	for(;;)
 	{
-		memset(buf, 0, linelimit);
-		ssize_t bread = getline(&buf, &sz, stdin);
 
+		ssize_t bread = getline(&buf, &linelen, stdin);
+		buf[bread] = 0;
 
 		if(bread >= 0)
 		{
 
-			if(sz > linelimit)
+			if(bread > linelimit)
 			{
 				printf("Input exceeds buffer limit.\n");
+				buf = trealloc(buf, linelimit);
+				memset(buf, 0, linelimit);
 				continue;
 			}
 
@@ -154,10 +226,10 @@ int main(void)
 					memcpy(cmdstr, buf + 1, pNL - buf - 1);
 				}
 				struct EditorCommand* pCmd = (struct EditorCommand*)
-					bsearch(cmdstr, commands, 3, sizeof(struct EditorCommand),
+					bsearch(cmdstr, commands, 4, sizeof(struct EditorCommand),
 						EditorCmdCmp);
 
-				if(pCmd && pCmd->cmdfp(&editor, &buf[1]) < 0)
+				if(pCmd && pCmd->cmdfp(&editor, &buf[1], bread - 1) < 0)
 				{
 					printf("*Quitting\n");
 					break;
@@ -165,7 +237,7 @@ int main(void)
  			}
 			else
 			{
-				LineEditor_Insert(&editor, buf, strnlen(buf, 256));
+				LineEditor_Append(&editor, buf, strnlen(buf, 256));
 			}
 		}
 		else
@@ -174,7 +246,7 @@ int main(void)
 			break;
 		}
 	}
-	free(buf);
+	tfree(buf);
 	LineEditor_Destroy(&editor);
 	return 0;
 }
